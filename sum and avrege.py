9135bf8d8b52101cb0517128be573a26f43a21a6
toxic123MMM <aashish02@gmail.com>
2024-09-06 13:00:02 +0400

    Add files via upload

---
+++ sum and avrege.py
@@ -0,0 +1,12 @@
+tree1=10
+tree2=20
+tree3=30
+tree4=40
+tree5=50
+#finding total of trees
+sum= tree1+tree2+tree3+tree4+tree5
+print("the sum of all the trees is ",sum)
+
+#finding avreage of trees
+average = sum/5
+print("average of all trees is ",average)
